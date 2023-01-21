e7fe5d27482e3b234bcc8df4bf5f25491f1e8d69
Allan Somera <nagal.somera@gmail.com>
2023-01-20 23:48:38 -0800

    learning '&' reference

--- cpp-heapPuzzles/puzzle2.cpp
+++ cpp-heapPuzzles/puzzle2.cpp
@@ -13,14 +13,19 @@ using std::endl;
 int main() {
   int *x = new int;
   int &y = *x;
-  y = 4;
+  int &c = y; //reference y, notice you dont need to dereference y, so what ever
+              //happens to y, will happen to c
+  y = 100;
 
   cout << &x << endl;
   cout << x << endl;
   cout << *x << endl;
 
   cout << &y << endl;
-  cout << y << endl;
+  cout << "y is: " << y << endl;
+
+  cout << &c << endl;
+  cout << "c is: " << c << endl;
   // cout << *y << endl;
 
   return 0;
