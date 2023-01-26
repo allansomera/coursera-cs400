e8e8227aec76c3d2f2fef079ac295893901afa36
Allan Somera <nagal.somera@gmail.com>
2023-01-25 19:19:19 -0800

    playing around with heap memory

--- cpp-heapMemory/heap1.cpp
+++ cpp-heapMemory/heap1.cpp
@@ -6,16 +6,40 @@
  */
 
 #include "Cube.h"
+#include <iostream>
 using uiuc::Cube;
 
-int main() {
-  int *p = new int;
-  Cube *c = new Cube;
 
+int main() {
+  int x = 11;
+  int *p = new int;
+  p = &x;
+  Cube *c = new Cube;
+  int *q = p;
+  std::cout << "updated p is: " << *p << std::endl;
+  std::cout << "q is: " << q << std::endl;
+  *q = 10;
+  std::cout << "update *q = 10 => " << *q << std::endl;
+  std::cout << "updated p is: " << *p << std::endl;
+  std::cout << "x is: " << x << std::endl;
+  std::cout << "changing q to nullptr" << std::endl;
+  // q = nullptr; 
+  delete q;
+  std::cout << "x is: " << x << std::endl;
+  std::cout << "delte q is: " << q << std::endl;
+  std::cout << "p is: " << *p << std::endl;
+  std::cout << "addressof p is: " << p << std::endl;
+
+  std::cout << "updating *p = 42 " << std::endl;
   *p = 42;
   (*c).setLength(4);
 
+  std::cout << "p is: " << *p << std::endl;
+  std::cout << "x is: " << x << std::endl;
+
   delete c;  c = nullptr;
-  delete p;  p = nullptr;
+  p = nullptr;
+  delete p;
+
   return 0;
 }
