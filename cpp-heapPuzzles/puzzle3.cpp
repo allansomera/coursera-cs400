727831d7f2bab7edca9fe998405c928dbd1784b6
Allan Somera <nagal.somera@gmail.com>
2023-01-29 23:31:58 -0800

    studied and answered and added comments to puzzle3.cpp

--- cpp-heapPuzzles/puzzle3.cpp
+++ cpp-heapPuzzles/puzzle3.cpp
@@ -11,16 +11,16 @@ using std::cout;
 using std::endl;
 
 int main() {
-  int *p, *q;
-  p = new int;
-  q = p;
-  *q = 8;
-  cout << *p << endl;
+  int *p, *q; //allocate stack memory for *p, *q
+  p = new int; // allocate heap memory for p to point at
+  q = p; // point q to the same place p is pointing at
+  *q = 8; // dereference q and store the value of 8, in turn p will also be pointing to a value 8
+  cout << *p << endl; // *p will print out 8
 
-  q = new int;
-  *q = 9;
-  cout << *p << endl;
-  cout << *q << endl;
+  q = new int; // make q point to a new heap memory location
+  *q = 9; //dereference q and store the value of 9 to the new location it is pointing at, p is still 8
+  cout << *p << endl; // 8
+  cout << *q << endl; // 9
 
   return 0;
 }
